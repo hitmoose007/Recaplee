2494310d80befca204e1caa99eea88d3477facca
Moosa Butt <moosabutt715@gmail.com>
2023-05-08 20:19:38 +0500

    minor tweak

--- src/components/ChangesBanner/ChangesBanner.tsx
+++ src/components/ChangesBanner/ChangesBanner.tsx
@@ -6,8 +6,8 @@ type Props = {};
 const QueryBanner = (props: Props) => {
   return (
     <div className="flex rounded-[30px]  bg-customBlue md:mt-4 md:space-x-4 md:px-8 md:py-4">
-      <ChangeCard topTitle="Detection of" bottomTitle="SERP Changes" />
-      <ChangeCard topTitle="Average of" bottomTitle="changes per webiste" />
+      <ChangeCard value={0}topTitle="Detection of" bottomTitle="SERP Changes" />
+      <ChangeCard value={0} topTitle="Average of" bottomTitle="changes per webiste" />
     </div>
   );
 };

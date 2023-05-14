96bacf688a160705a2867b77d6bbcde90fa14fed
Moosa Butt <moosabutt715@gmail.com>
2023-05-14 17:58:42 +0500

    pages removed

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,7 +6,9 @@ import DesktopBanner from '@/components/Layout/DesktopBanner';
 import MobileBanner from '@/components/Layout/MobileBanner';
 import { PageContext } from '@/context/PageContext';
 import { PageView } from '@/utils/enums';
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import Login from '@/components/Login';
 const nunito = Nunito({
   subsets: ['cyrillic'],
   variable: '--font-nunito',
@@ -14,15 +16,20 @@ const nunito = Nunito({
 
 function Layout({ children }: { children: React.ReactNode }) {
   const isMobile = useMobileStatus();
+  const session = useSession();
 
   const [page, setPage] = useState<PageView>(PageView.DASHBOARD);
+
+  if (session) {
+    return <Login />;
+  }
   return (
     <>
       <div className={`${nunito.variable} font-sans `}>
         <PageContext.Provider value={{ page, setPage }}>
           {!isMobile && <DesktopBanner />}
           {isMobile && <MobileBanner />}
-          <div className=" mx-6 md:mx-10 my-8">
+          <div className=" mx-6 my-8 md:mx-10">
             <div className="md:mb-1">{!isMobile && <BackButton />}</div>
             <main>{children}</main>
           </div>
---
+++ src/components/Login.tsx
@@ -0,0 +1,39 @@
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import Image from 'next/image';
+const Login = () => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
+  return (
+    <div className='h-screen w-screen bg-slate-100'>
+    <div className="flex flex-col  items-center justify-center py-52 ">
+        <Image 
+        src="/logo.svg"
+        alt="Picture of the author"
+        width={400}
+        height={400}
+
+        className="bg-black rounded-[30px] px-10 py-10"
+        />
+
+      <div className="" style={{ padding: '50px 0 100px 0' }}>
+        {session ? (
+          <Auth
+            providers={['google']}
+            onlyThirdPartyProviders={true}
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            theme="dark"
+          />
+        ) : (
+          <p>Account page will go here.</p>
+        )}
+      </div>
+    </div>
+</div>
+  );
+};
+
+export default Login;

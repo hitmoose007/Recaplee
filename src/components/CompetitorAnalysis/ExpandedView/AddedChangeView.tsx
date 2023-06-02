74231591ebf08fefb7d566a648c23be55cf4e452
Moosa Butt <moosabutt715@gmail.com>
2023-06-02 22:57:53 +0500

    additionalUtilities added to all views

--- src/components/CompetitorAnalysis/ExpandedView/AddedChangeView.tsx
+++ src/components/CompetitorAnalysis/ExpandedView/AddedChangeView.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Change } from '@/types/my-types';
 import HeaderTagView from '../HeaderTagView';
+import AdditionalUtilitiesGroup from './AdditionalUtilitiesGroup';
 type Props = {
   index: number;
   AddedChange: Change;
@@ -10,17 +11,22 @@ const AddedChangeView = ({ index, AddedChange }: Props) => {
   return (
     <div key={index} className="flex space-x-8 md:space-x-20">
       <div
-        className={` pointer-events-none  w-full rounded-[30px] bg-[#E0E8F0]  p-2    text-sm font-bold  md:space-x-4  md:space-y-4 `}
+        className={`flex w-1/2 justify-between rounded-[30px] bg-[#E0E8F0]  px-4 py-2   text-sm font-bold  md:space-x-4  `}
       >
         <p className={'invisible'}>{AddedChange?.value}</p>
+        <span className="text-[9px] text-customGray">#{index+1}</span>
       </div>
       <div
-        className={` pointer-events-none w-full rounded-[30px] bg-white  p-2    text-sm font-bold text-customGreen md:space-x-4  md:space-y-4 `}
+        className={` flex w-1/2 justify-between rounded-[30px] text-customGreen bg-white  px-4 py-2   text-sm font-bold  md:space-x-4  `}
       >
         <p>
           <HeaderTagView tag={AddedChange?.tag || ''} />
           {AddedChange?.value}
         </p>
+        <AdditionalUtilitiesGroup
+          index={index}
+          toBeCopiedText={AddedChange?.value}
+        />
       </div>
     </div>
   );
